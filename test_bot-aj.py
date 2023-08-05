eef1edcb04fa2e681303d7cba74b4698ca47ca14
AJ <akinjcampbell@gmail.com>
2023-08-05 12:44:36 -0500

    Add buy price

--- test_bot-aj.py
+++ test_bot-aj.py
@@ -11,8 +11,12 @@ coinbase_API_key = '9xF36UsqaybNxP2M'
 coinbase_API_secret = '6NXAzklQF2BwrlQivB0t6alFOmBXuqfW'
 client = Client(coinbase_API_key, coinbase_API_secret)
 # OAuth made user specific methods work
-client = OAuthClient("16da44729b0f8fd1009ed6f20d0ecdb5c16ebdadd763a9c70d74735314ee680d", "16da44729b0f8fd1009ed6f20d0ecdb5c16ebdadd763a9c70d74735314ee680d") 
-print(client.get_current_user())
+client = OAuthClient("28f70e7f2ea61ac9612c535eccd1a78277f02ac42e921729806e2e80bba63f40", "28f70e7f2ea61ac9612c535eccd1a78277f02ac42e921729806e2e80bba63f40") 
+user = client.get_current_user()
+# Get buy price
+buy_price = client.get_buy_price(currency_pair = 'BTC-USD')
+sell_price = client.get_sell_price(currency_pair = 'BTC-USD')
+print(buy_price, sell_price)
 
 
 
