9d6f0f18d573498d5c9f75f429a52e4ef1259be3
AJ <akinjcampbell@gmail.com>
2023-08-06 11:42:14 -0500

    Need environment variable

--- test_bot-aj.py
+++ test_bot-aj.py
@@ -7,8 +7,9 @@ print("hello")
 
 # Client instatiation and Coinbase information
 # To generate API key visit https://help.coinbase.com/en/exchange/managing-my-account/how-to-create-an-api-key
-coinbase_API_key = '9xF36UsqaybNxP2M'
-coinbase_API_secret = '6NXAzklQF2BwrlQivB0t6alFOmBXuqfW'
+# Add api key and api secret environment variables
+coinbase_API_key = Environment_Variable
+coinbase_API_secret = Environment_Variable
 client = Client(coinbase_API_key, coinbase_API_secret)
 # OAuth made user specific methods work
 client = OAuthClient("08e141e2ab09c8ddaa7275db0f126e0875c1bc4a5fa0784f53a69378cf0cebff", "08e141e2ab09c8ddaa7275db0f126e0875c1bc4a5fa0784f53a69378cf0cebff") 
