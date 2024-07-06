7a7a1bd07b8fe3cb25da777c591ab6cbfa159c43
AJ <akinjcampbell@gmail.com>
2024-07-06 16:04:41 -0500

    add some code (see chatGPT)

--- bitbot-botbit.py
+++ bitbot-botbit.py
@@ -1,2 +1,14 @@
+# Install Required Libraries: Install cbpro (Coinbase Pro API client) and any other necessary libraries.
+pip install cbpro
 
+# Setup the Environment: Import the necessary modules and set up your API keys.
+import cbpro
+import time
+
+API_KEY = 'your_api_key'
+API_SECRET = 'your_api_secret'
+API_PASS = 'your_api_passphrase'
+
+# Initialize the Client:
+client = cbpro.AuthenticatedClient(API_KEY, API_SECRET, API_PASS)
 
