5a6489b3e3a22c04d807915a1a7d822fbb021587
AJ <akinjcampbell@gmail.com>
2024-07-21 17:12:11 -0500

    added first section of code. error line 4

--- bitbot-botbit.py
+++ bitbot-botbit.py
@@ -1,1 +1,9 @@
+#BitBot-BotBit Program | Start: 7/21/24 | 
 
+#Setup the Environment: Import the necessary modules and set up your API keys.
+import cbpro
+# import time
+
+# API_KEY = 'your_api_key'
+# API_SECRET = 'your_api_secret'
+# API_PASS = 'your_api_passphrase'
